3a5ec851cedf9b711a45e3eeff0dec24b6bdffdc
Adrián <135874675+Madrix5@users.noreply.github.com>
2024-01-06 19:24:17 +0100

    ft_strlcpy terminado

---
+++ ft_strlcpy.c
@@ -0,0 +1,13 @@
+int ft_strlcpy(char *src, char *dest)
+{
+    int i;
+
+    i = 0;
+    while (src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return (i);
+}
