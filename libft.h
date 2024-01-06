dd5b240891bb6697e38a9e79f9137ee9ee8ce82b
Adrián <135874675+Madrix5@users.noreply.github.com>
2024-01-06 22:04:06 +0100

    creado libft.h

---
+++ libft.h
@@ -0,0 +1,8 @@
+int ft_isalnum(char c);
+int ft_isalpha(char c);
+int ft_isascii(char c);
+int ft_isdigit(char c);
+int ft_isprint(char c);
+char *ft_memset(char *point, char c, int n);
+int ft_strlcpy(char *src, char *dest);
+int ft_strlen(char *str);
