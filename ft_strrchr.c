e45940c3455630a8b36a1aa06e7dbd799cbd4fcc
Adrian Jimenez Calderon <adrijime@car1s2.42barcelona.com>
2024-01-26 16:06:20 +0100

    Corregido | PAco pasado

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: adrijime <adrijime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 12:45:20 by adrijime          #+#    #+#             */
-/*   Updated: 2024/01/21 15:17:57 by adrijime         ###   ########.fr       */
+/*   Updated: 2024/01/26 16:05:35 by adrijime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,9 @@ char	*ft_strrchr(const char *s, int c)
 	i = ft_strlen(s);
 	while (i >= 0)
 	{
-		if (s[i] == c)
+		if (s[i] == (unsigned char)c)
 			return ((char *)&s[i]);
 		i--;
 	}
-	if (s[i] == c)
-		return ((char *)&s[i]);
 	return (NULL);
 }
