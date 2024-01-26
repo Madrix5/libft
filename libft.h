f36fae50729ad4e4a0f004d0ad17b000adcda56a
Adrián <135874675+Madrix5@users.noreply.github.com>
2024-01-26 22:44:39 +0100

    Añadido lstnew y struct

--- libft.h
+++ libft.h
@@ -62,11 +62,14 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-//----------------------BONUS----------------------------------//
+//----------------------STRUCT----------------------------------//
 typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
 }	t_list;
+
+//-----------------------BONUS----------------------------------//
+
 
 #endif
