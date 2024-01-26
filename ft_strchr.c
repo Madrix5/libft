5a559806abfe48e7dbd107dff98f7256b14696e1
Adrián <135874675+Madrix5@users.noreply.github.com>
2024-01-26 13:08:30 +0100

    Update ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -17,15 +17,15 @@ char	*ft_strchr(const char *s, int c)
 	int		i;
 
 	i = 0;
-	while (s[i] != '\0')
+	while (s[i])
 	{
-		if (s[i] == c)
+		if (s[i] == ((char)c))
 		{
 			return ((char *)&s[i]);
 		}
 		i++;
 	}
-	if (s[i] == c)
+	if ((char)c == '\0')
 		return ((char *)&s[i]);
 	return (NULL);
 }
