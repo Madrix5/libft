3a69e229a1b5761d7a33f9fabcc73c7acec9ff75
Adrian Jimenez Calderon <adrijime@car8s2.42barcelona.com>
2024-01-27 14:19:04 +0100

    Aniadidos todos los prototipos del bonus

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: adrijime <adrijime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 11:25:06 by adrijime          #+#    #+#             */
-/*   Updated: 2024/01/26 20:45:26 by adrijime         ###   ########.fr       */
+/*   Updated: 2024/01/27 14:16:02 by adrijime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,8 +68,15 @@ typedef struct s_list
 	void			*content;
 	struct s_list	*next;
 }	t_list;
-
 //-----------------------BONUS----------------------------------//
-
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list	*ft_lstnew(void *content);
+int		ft_lstsize(t_list *lst);
 
 #endif
